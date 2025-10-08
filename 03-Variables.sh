cf0f6f8f6b6b075de94c80abdb610809e917d410
Sandeep Chukkapalli <sandeep.chukkapalli@gmail.com>
2025-10-08 11:01:32 +0530

    introducing variables

---
+++ 03-Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# No space before or after equal
+PERSON1=John
+PERSON2=Raheem
+
+
+echo "$PERSON1:: Hi $PERSON2 , How are you?"
+echo "$PERSON2:: Hi $PERSON1 , I am fine"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting now a days"
